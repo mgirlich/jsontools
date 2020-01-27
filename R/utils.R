80d66257fbdc444dc528a5295d828281f7bab918
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-01-27 09:41:33 +0100

    better error message for invalid json

--- R/utils.R
+++ R/utils.R
@@ -13,11 +13,29 @@ stop_jsontools <- function(error_type, ...) {
 
 #' @export
 conditionMessage.jsontools_error_invalid_json <- function(cnd) {
-  size <- min(length(cnd$errors), 6)
-  paste0(
-    "invalid json\n",
-    glue::glue_data(cnd, "{locations}: offset {offsets}\n{errors}")[1:size]
-  )
+  errors_shown <- 5
+
+  n_invalid <- length(cnd$offsets)
+  if (n_invalid > 10) {
+    locs <- c(head(cnd$locations, 5), "...", tail(cnd$locations, 5))
+  } else {
+    locs <- cnd$locations
+  }
+  locs <- paste0(locs, collapse = ", ")
+
+  head <- glue("
+  invalid JSON at {n_invalid} locations:
+    {locs}
+  ")
+
+  size <- min(length(cnd$errors), errors_shown)
+  body <- glue::glue_data(cnd, "{locations}: offset {offsets}\n{errors}")[1:size]
+  if (n_invalid > errors_shown) {
+    body <- c(body, paste0("... (", n_invalid - size, " more errors)"))
+  }
+  body <- paste0(body, collapse = "\n")
+
+  paste0(head, "\n\n", body)
 }
 
 
