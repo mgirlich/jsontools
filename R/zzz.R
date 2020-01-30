b93dee51308cfa68225c3dce13255aa872b90dcd
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-01-30 13:08:34 +0100

    bugfix: fixed_base_scalar not found

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,6 @@
   s3_register("pillar::is_vector_s3", "json2")
 
   if (is_installed("dbplyr")) {
-    assign_in_dbplyr("base_scalar", fixed_base_scalar)
     assign_in_dbplyr("sql_translate_env.PqConnection", fixed_pq_translator)
   }
 
