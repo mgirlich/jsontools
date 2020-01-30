71868cb70b9eb3ae4b3322f3ef140b50b9960078
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-01-30 13:07:44 +0100

    better error message for json_build_object

--- R/build_object.R
+++ R/build_object.R
@@ -6,6 +6,8 @@
 #'   f = .foo, bf1 = `.bar.foo | .[1]`, bb = `.bar.bar`
 #' )
 json_build_object <- function(x, ...) {
+  force(x)
+
   jqr_ob <- jqr::build_object(x, ...)
   jq_do(x, jqr_ob$args[[1]], .na = NA_character_)
 }
