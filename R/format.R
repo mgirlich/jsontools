c64b959253beb307d7060fae6b0ff2024cc46f30
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-02-04 09:16:31 +0100

    refactor format

--- R/format.R
+++ R/format.R
@@ -1,10 +1,6 @@
 #' @export
 format.json2 <- function(x, ..., pretty = FALSE) {
   # TODO by default don't print too many elements?
-  if (length(x) == 0) {
-    return(character())
-  }
-
   if (is_true(pretty)) {
     x <- prettify(x)
   }
@@ -14,31 +10,10 @@ format.json2 <- function(x, ..., pretty = FALSE) {
 
 
 #' @export
-obj_print_data.json2 <- function(x, ..., pretty = FALSE) {
-  cat(x)
-  # if (is_true(pretty)) {
-  #   cat(format(x, pretty = TRUE))
-  # } else {
-  #   out <- stats::setNames(format(x), names(x))
-  #   print(out, quote = FALSE)
-  # }
-
+print.json2 <- function(x, ...) {
+  cat(x, ..., sep = "\n")
   invisible(x)
 }
-# obj_print_data.json2 <- function(x, ..., pretty = FALSE) {
-#   if (length(x) == 0) {
-#     return()
-#   }
-#
-#   if (is_true(pretty)) {
-#     cat(format(x, pretty = TRUE))
-#   } else {
-#     out <- stats::setNames(format(x), names(x))
-#     print(out, quote = FALSE)
-#   }
-#
-#   invisible(x)
-# }
 
 #' @export
 vec_ptype_abbr.json2 <- function(x) {
@@ -50,18 +25,9 @@ vec_ptype_full.json2 <- function(x) {
   "json2"
 }
 
-# Dynamically exported, see zzz.R
-is_vector_s3.json2 <- function(x) TRUE
-
-
 #' @export
 pillar_shaft.json2 <- function(x, ...) {
-  # out <- ifelse(
-  #   is.na(x),
-  #   NA_character_,
-  #   paste0(pillar::style_subtle("<raw "), blob_size(x, ...), pillar::style_subtle(">"))
-  # )
-
-  out <- noquote(x)
+  out <- noquote(format(x))
+  out[is.na(x)] <- NA
   pillar::new_pillar_shaft_simple(out, align = "right")
 }
