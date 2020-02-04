f9744d641eb3d192993520601375c391601859a1
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-02-04 09:20:55 +0100

    remove unnecessary is_vector_s3

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,7 @@
 .onLoad <- function(...) {
   s3_register("pillar::pillar_shaft", "json2")
-  s3_register("pillar::is_vector_s3", "json2")
 
   if (is_installed("dbplyr")) {
     assign_in_dbplyr("sql_translate_env.PqConnection", fixed_pq_translator)
   }
-
-  invisible()
 }
