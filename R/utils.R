a91d5c98ad85f88fb0769da11dc3c4f70f9118ae
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-12-18 12:49:41 +0000

    add `indent` argument to `json_prettify()`

--- R/utils.R
+++ R/utils.R
@@ -16,20 +16,20 @@ check_present <- function(x) {
 #' x <- c("[1,2,   3]", '{"a": 1, "b": 2}')
 #' json_prettify(x)
 #' json_minify(x)
-json_prettify <- function(x) {
-  json2_do(x, jsonlite::prettify, json2 = json2)
+json_prettify <- function(x, indent = 3) {
+  json2_do(x, jsonlite::prettify, indent = indent, json2 = TRUE)
 }
 
 #' @rdname json_prettify
 #' @export
 json_minify <- function(x) {
-  json2_do(x, jsonlite::minify, json2 = json2)
+  json2_do(x, jsonlite::minify, json2 = TRUE)
 }
 
-json2_do <- function(x, f, json2 = TRUE) {
+json2_do <- function(x, f, json2 = TRUE, ...) {
   x <- as.character(x)
   x_na <- is.na(x)
-  x[!x_na] <- as.character(lapply(x[!x_na], f))
+  x[!x_na] <- as.character(lapply(x[!x_na], f, ...))
 
   if (is_true(json2)) {
     new_json2(x)
