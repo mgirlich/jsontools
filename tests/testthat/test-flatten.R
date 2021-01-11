40a8330567c1a33e9f702d498fb33e3b418e8c3c
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-01-11 14:24:10 +0000

    add tests for `json_unnest_wider()`

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -306,3 +306,122 @@ test_that("json_unnest_longer with discog_json", {
     style = "json2"
   )
 })
+
+
+# json_unnest_wider -------------------------------------------------------
+
+test_that("json_unnest_wider works", {
+  df <- tibble(
+    id = 1:2,
+    json = c(
+      '{"a": null, "b": 12}',
+      '{"a": 21}'
+    )
+  )
+
+  expect_equal(
+    json_unnest_wider(df, "json"),
+    tibble(
+      id = c(1, 2),
+      b = c(12, NA),
+      a = c(NA, 21)
+    )
+  )
+})
+
+test_that("json_unnest_wider handles NA", {
+  skip("not yet decided what to do")
+  df <- tibble(
+    id = 1:2,
+    json = c(
+      '{"a": 1}',
+      'null'
+    )
+  )
+
+  expect_equal(
+    json_unnest_wider(df, "json"),
+    tibble(
+      id = c(1, 2),
+      a = c(1, NA)
+    )
+  )
+
+  df <- tibble(
+    id = 1:2,
+    json = c(
+      NA,
+      '{"a": 1}'
+    )
+  )
+
+  expect_equal(
+    json_unnest_wider(df, "json"),
+    tibble(
+      id = 2,
+      a = 1
+    )
+  )
+})
+
+
+test_that("json_unnest_wider works", {
+  expect_named(
+    json_unnest_wider(
+      tibble(json = '{"b": 2, "a": 1}'),
+      "json",
+      names_sort = TRUE
+    ),
+    c("a", "b")
+  )
+})
+
+test_that("json_unnest_wider respects ptype", {
+  expect_equal(
+    tibble(
+      json = '{"chr": "a", "dbl": 1, "int": true}'
+    ) %>%
+      json_unnest_wider(
+        "json",
+        ptype = list(
+          int = double(),
+          lgl = integer()
+        )
+      ),
+    tibble(
+      chr = "a",
+      dbl = 1,
+      int = 1
+    )
+  )
+})
+
+test_that("json_unnest_wider errors on non-objects", {
+  expect_snapshot_error(
+    json_unnest_wider(tibble(json = '[1]'), "json")
+  )
+})
+
+test_that("json_unnest_wider with discog_json", {
+  df <- tibble(json = discog_json)
+
+  item_df <- json_unnest_longer(
+    df,
+    "json",
+    values_to = "item"
+  )[1:3, ]
+
+  expect_snapshot_value(
+    item_df,
+    style = "json2"
+  )
+
+  expect_snapshot_value(
+    json_unnest_wider(
+      item_df,
+      "item",
+      path = c("$.basic_information")
+    ),
+    style = "json2"
+  )
+})
