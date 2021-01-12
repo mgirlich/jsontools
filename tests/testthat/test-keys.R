4f39135050b0bfcc0b30b1260f7cfe078ac88f54
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-01-12 08:22:30 +0000

    remove unnecessary code

--- tests/testthat/test-keys.R
+++ tests/testthat/test-keys.R
@@ -28,32 +28,3 @@ test_that("json_path_exists works", {
     c(TRUE, FALSE, FALSE, NA)
   )
 })
-
-
-# x1 <- c('{"a": {"x": 1}, "b": 2, "c": 3}')
-# x2 <- c('{"a": {"x": 11, "y": 22}, "s": 12, "t": [1, 2, 3]}')
-# x <- c(x1, x2)
-#
-# test_that("jsonr_has_keys works", {
-#   expect_equal(
-#     json_path_exists(x, c("b", "s")),
-#     tibble(b = c(TRUE, FALSE), s = c(FALSE, TRUE))
-#   )
-#
-#   expect_equal(
-#     jsonr_has_keys(NA_character_, c("b", "s")),
-#     tibble(b = NA, s = NA)
-#   )
-# })
-#
-# test_that("jsonr_has_keys1 works", {
-#   expect_equal(
-#     jsonr_has_keys1(x[1], c("b", "s")),
-#     c(b = TRUE, s = FALSE)
-#   )
-#
-#   expect_equal(
-#     jsonr_has_keys1(NA_character_, c("b", "s")),
-#     c(b = NA, s = NA)
-#   )
-# })
