70940704c4fca7e1b35d6e3e35a5556bb1f73cfc
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-01-20 10:18:31 +0000

    add cast and coerce methods

--- R/cast.R
+++ R/cast.R
@@ -9,14 +9,44 @@
 #' @export
 #' @export vec_cast.json2
 vec_cast.json2 <- function(x, to, ...) UseMethod("vec_cast.json2")
+#' @noRd
+#' @export
+vec_cast.json2_object <- function(x, to, ...) UseMethod("vec_cast.json2_object")
+#' @noRd
+#' @export
+vec_cast.json2_array <- function(x, to, ...) UseMethod("vec_cast.json2_array")
 
 # self cast
 #' @export
 vec_cast.json2.json2 <- function(x, to, ...) x
+#' @export
+vec_cast.json2_object.json2_object <- function(x, to, ...) x
+#' @export
+vec_cast.json2_array.json2_array <- function(x, to, ...) x
+
+# cast to json2
+#' @export
+vec_cast.json2.json2_object <- function(x, to, ...) new_json2(x)
+#' @export
+vec_cast.json2.json2_array <- function(x, to, ...) new_json2(x)
+
+# cast to object/array
+#' @export
+vec_cast.json2_object.json2 <- function(x, to, ...) {
+  stop("not yet implemented")
+}
+#' @export
+vec_cast.json2_array.json2 <- function(x, to, ...) {
+  stop("not yet implemented")
+}
 
 # cast to character
 #' @export
 vec_cast.character.json2 <- function(x, to, ...) vec_data(x)
+#' @export
+vec_cast.character.json2_object <- function(x, to, ...) vec_data(x)
+#' @export
+vec_cast.character.json2_array <- function(x, to, ...) vec_data(x)
 
 #' @export
 vec_cast.list.json2 <- function(x, to, ..., x_arg = "x", to_arg = "to") {
@@ -29,6 +59,21 @@ vec_cast.json2.character <- function(x, to, ...) {
   # workaround for problem with rendering in markdown
   x <- ifelse(x == "__NA__", NA, x)
   json2(vec_data(x))
+}
+#' @export
+vec_cast.json_array.character <- function(x, to, ...) {
+  # stop("not yet implemented")
+  if (!grepl("^\\s.\\[", x)) {
+    abort("`x` must be a JSON array.")
+  }
+  new_json_array(json2(vec_data(x)))
+}
+#' @export
+vec_cast.json_object.character <- function(x, to, ...) {
+  if (!grepl("^\\s.\\[", x)) {
+    abort("`x` must be a JSON object.")
+  }
+  new_json_object(json2(vec_data(x)))
 }
 
 # json classes from other packages ----------------------------------------
--- R/coerce.R
+++ R/coerce.R
@@ -8,6 +8,14 @@
 #' @export
 #' @export vec_ptype2.json2
 vec_ptype2.json2 <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_ptype2.json2", y)
+#' @noRd
+#' @method vec_ptype2 json2_object
+#' @export
+vec_ptype2.json2_object <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_ptype2.json2_object", y)
+#' @noRd
+#' @method vec_ptype2 json2_array
+#' @export
+vec_ptype2.json2_array <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_ptype2.json2_array", y)
 
 #' @method vec_ptype2.json2 default
 #' @export
@@ -15,13 +23,39 @@ vec_ptype2.json2.default <- function(x, y, ..., x_arg = "x", y_arg = "y") {
   vec_default_ptype2(x, y, x_arg = x_arg, y_arg = y_arg)
 }
 
+# self coercion
 #' @method vec_ptype2.json2 json2
 #' @export
 vec_ptype2.json2.json2 <- function(x, y, ...) new_json2()
+#' @export
+vec_ptype2.json2_object.json2_object <- function(x, y, ...) new_json_object()
+#' @export
+vec_ptype2.json2_array.json2_array <- function(x, y, ...) new_json_array()
+
+# json2-object/array
+#' @export
+vec_ptype2.json2_object.json2 <- function(x, y, ...) new_json2()
+#' @export
+vec_ptype2.json2.json2_object <- function(x, y, ...) new_json2()
+#' @export
+vec_ptype2.json2_array.json2 <- function(x, y, ...) new_json2()
+#' @export
+vec_ptype2.json2.json2_array <- function(x, y, ...) new_json2()
+
+# object/array
+#' @export
+vec_ptype2.json2_array.json2_object <- function(x, y, ...) new_json2()
+#' @export
+vec_ptype2.json2_object.json2_array <- function(x, y, ...) new_json2()
 
 #' @export
 vec_ptype2.json2.character <- function(x, y, ...) character()
 vec_ptype2.character.json2 <- function(x, y, ...) character()
+
+#' @export
+vec_ptype_full.json2_object <- function(x) "json2_object"
+#' @export
+vec_ptype_full.json2_array <- function(x) "json2_array"
 
 
 # json classes from other packages ----------------------------------------
