0f0808e5847e897426819fd10f476ff5c99aaedd
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-01-22 09:28:32 +0000

    clean up and document data

---
+++ R/got_chars.R
@@ -0,0 +1,20 @@
+#' Game of Thrones POV characters
+#'
+#' A JSON with some basic information about some of the Game of Thrones
+#' characters.
+#'
+#' @format A JSON array of objects. Each object describes a character and has
+#'   the following keys:
+#'   * `url`
+#'   * `id`
+#'   * `name`
+#'   * `alive`
+#'   * `titles`
+#'   * `aliases`
+#'   * `allegiances`
+#'
+#' @export
+#' @source <https://anapioficeandfire.com>
+#' @examples
+#' got_chars_json
+"got_chars_json"
