f1afb3f087f4cf9bf0d887cfdd4301e4b354c6f0
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-01-26 09:18:41 +0000

    remove unused `sql_with_vals()`

--- R/sqlite_utils.R
+++ R/sqlite_utils.R
@@ -10,25 +10,3 @@ write_json_tbl <- function(x, ...) {
 exec_sqlite_json <- function(sql) {
   tibble::as_tibble(DBI::dbGetQuery(con, sql))
 }
-
-#' @noRd
-#' @examples
-#' values <- tibble::tibble(
-#'   id = 1:3,
-#'   letters = letters[1:3],
-#'   json = x
-#' )
-#' sql_with_vals(values)
-sql_with_vals <- function(values) {
-  sql_values <- DBI::sqlData(con, values, row.names = FALSE)
-  fields <- DBI::dbQuoteIdentifier(con, names(sql_values))
-
-  # Convert fields into a character matrix
-  rows <- do.call(paste, c(sql_values, sep = ", "))
-  DBI::SQL(paste0(
-    "WITH T(", paste(fields, collapse = ", "), ") AS (\n",
-    "VALUES\n",
-    paste0("  (", rows, ")", collapse = ",\n"),
-    ")"
-  ))
-}
