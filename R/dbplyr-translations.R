5b6fb3824c48cd7bb79d51b04fa636950c95d590
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-03-26 08:00:05 +0000

    hacky translation for `json_flatten()` and `json_extract()`

--- R/dbplyr-translations.R
+++ R/dbplyr-translations.R
@@ -1,35 +1,54 @@
-assign_in_dbplyr <- function(name, f) {
+assign_in_dbplyr <- function(f) {
   # hack: need to assign sql_translate_env here so that assignInNamespace doesn't complain...
-  sql_translate_env <- function(con) {
-    UseMethod("sql_translate_env")
+  if (utils::packageVersion("dbplyr") >= "2.0.0") {
+    sql_translation <- function(con) {
+      UseMethod("sql_translate_env")
+    }
+
+    name <- "sql_translation.PqConnection"
+  } else {
+    sql_translate_env <- function(con) {
+      UseMethod("sql_translate_env")
+    }
+
+    name <- "sql_translate_env.PqConnection"
   }
 
   ns <- getNamespace("dbplyr")
   unlockBinding(name, ns)
   # assign(name, f, envir = ns)
   # assignInNamespace(name, f, ns = ns)
-  assignInNamespace(name, f, ns = "dbplyr")
+  utils::assignInNamespace(name, f, ns = "dbplyr")
   lockBinding(name, ns)
 }
 
 
-# x <- c(".a", ".a.b", '.["a b"].[2]')
-jq_index_to_pg_path <- function(path) {
-  jq_validate_path(path)
-  # \.(\w|\[".+?"\]|\[\d+])
-  matches1 <- stringr::str_match_all(path, '\\.(\\w+|\\[".+?"\\]|\\[\\d+?])')[[1]]
+# path <- c("$.a", "$.a.b", '$.["a b"].[2]')
+# jsonpath_to_pg_path(path[1])
+# jsonpath_to_pg_path(path[2])
+# jsonpath_to_pg_path(path[3])
+jsonpath_to_pg_path <- function(path) {
+  matches1 <- stringr::str_match_all(path, r'{\.(\w+|\[".+?"\]|\[\d+?])}')[[1]]
   matches2 <- stringr::str_match(matches1[, 2], '^\\[?\\"?(.+?)\\"?\\]?$')[, 2]
   paste0("{", paste0(matches2, collapse = ","), "}")
 }
 
-postgres_translator <- dbplyr:::sql_translate_env.PqConnection()
-
 # update translators
-scalar_new <- postgres_translator$scalar
-aggregate_new <- postgres_translator$aggregate
-window_new <- postgres_translator$window
+cast_to_ptype <- function(ptype, sql) {
+  # TODO improve this ugly hack
+  switch(class(ptype)[[1]],
+    integer = dbplyr::translate_sql(as.integer({!!sql})),
+    numeric = dbplyr::translate_sql(as.double({!!sql})),
+    Date = dbplyr::translate_sql(as.Date({!!sql})),
+    POSIXct = dbplyr::translate_sql(as_datetime({!!sql})),
+    sql
+  )
+}
 
 fixed_pq_translator <- function(con) {
+  postgres_translator <- dbplyr:::sql_translation.PqConnection()
+  scalar_new <- postgres_translator$scalar
+
   scalar_new$jsonr_has_key = function(x, key) {
     dbplyr::build_sql(!!x, " ? ", key)
   }
@@ -38,43 +57,72 @@ fixed_pq_translator <- function(con) {
     dbplyr::translate_sql(!is.na(json_extract(x, path)))
   }
 
-  scalar_new$json_extract = function(x, path) {
-    path <- jq_index_to_pg_path(path)
-    dbplyr::build_sql("(", !!x, "#>", path, ")")
+  scalar_new$json_flatten = function(x, ptype, allow_scalars = FALSE, wrap_scalars = FALSE, bigint_as_char = TRUE) {
+    if (is_true(allow_scalars)) {
+      flatten_sql <- dbplyr::translate_sql(json_array_elements(x))
+
+      flatten_sql <- dbplyr::translate_sql(
+        ifelse(
+          json_typeof(x) %in% c("string", "number", "boolean", "null"),
+          x,
+          !!flatten_sql
+        )
+      )
+
+      flatten_sql <- dbplyr::translate_sql(as.character(!!flatten_sql))
+    } else {
+      flatten_sql <- dbplyr::translate_sql(json_array_elements_text(x))
+    }
+
+    if (is_true(wrap_scalars)) {
+      # TODO wrap_scalars?
+      abort("`wrap_scalars` is not supported")
+    }
+
+    cast_to_ptype(ptype, flatten_sql)
   }
 
-  scalar_new$jsonr_extract_chr = function(x, path) {
-    path <- jq_index_to_pg_path(path)
-    dbplyr::build_sql("(", !!x, "#>>", path, ")")
-  }
+  scalar_new$json_extract = function(x, path, ptype, default = NULL, na = NA, wrap_scalars = FALSE, bigint_as_char) {
+    if (is_json2(ptype)) {
+      # extract as JSON
+      extract_sql <- dbplyr::translate_sql(x %#>% !!path)
+      # could use other function:
+      # * `json_extract_path()`: uses a variadic list for the path
+      # * `jsonb_path_query()`: uses json path language
+      # see: https://www.postgresql.org/docs/current/functions-json.html#FUNCTIONS-JSON-PROCESSING-TABLE
+    } else {
+      # extract as text
+      extract_sql <- dbplyr::translate_sql(x %#>>% !!path)
+    }
 
-  scalar_new$jsonr_extract_lgl = function(x, path) {
-    dbplyr::translate_sql(as.numeric(jsonr_extract_chr(!!x, !!path)))
-  }
+    if (!is.null(default)) {
+      extract_sql <- dbplyr::translate_sql(coalesce(!!extract_sql, !!default))
+    }
 
-  scalar_new$jsonr_extract_int = function(x, path) {
-    dbplyr::translate_sql(as.integer(jsonr_extract_chr(!!x, !!path)), con = con)
-  }
+    if (!is.na(na) && !is.null(default)) {
+      extract_sql <- dbplyr::translate_sql(ifelse(
+        is.na(x),
+        na,
+        !!extract_sql
+      ))
+    }
 
-  scalar_new$jsonr_extract_dbl = function(x, path) {
-    dbplyr::translate_sql(as.numeric(jsonr_extract_chr(!!x, !!path)))
-  }
+    if (is_true(wrap_scalars)) {
+      extract_sql <- dbplyr::translate_sql(
+        ifelse(
+          type_of(!!extract_sql) %in% !!c("object", "array"),
+          !!extract_sql,
+          json_build_array(!!extract_sql)
+        )
+      )
+    }
 
-  scalar_new$jsonr_extract_dat = function(x, path, format = "%Y-%m-%d") {
-    dbplyr::translate_sql(TO_DATE(jsonr_extract_chr(!!x, !!path), !!format))
-  }
-
-  scalar_new$jsonr_extract_dtt <- function(x, path, format = "%Y-%m-%d %H:%M:%S") {
-    dbplyr::translate_sql(TO_TIMESTAMP(jsonr_extract_chr(!!x, !!path), !!format))
-  }
-
-  scalar_new$jsonr_extract_lst = function(x, path) {
-    stop("not supported")
+    cast_to_ptype(ptype, extract_sql)
   }
 
   dbplyr::sql_variant(
     scalar = scalar_new,
-    aggregate = aggregate_new,
-    window = window_new
+    aggregate = postgres_translator$aggregate,
+    window = postgres_translator$window
   )
 }
--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,7 @@ jsontools_env <- environment()
   # s3_register("pillar::pillar_shaft", "json2")
 
   assign("con", DBI::dbConnect(RSQLite::SQLite(), ":memory:"), envir = jsontools_env)
+  assign_in_dbplyr(fixed_pq_translator)
 }
 
 .onUnload <- function(...) {
